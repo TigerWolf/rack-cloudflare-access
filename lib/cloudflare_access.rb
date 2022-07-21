f751ad2f56edb6fafcb3de54a2dd3da782d88788
Kieran Andrews <TigerWolf@users.noreply.github.com>
2022-07-21 13:28:00 +0930

    Improve documentation and cleanup

--- lib/cloudflare_access.rb
+++ lib/cloudflare_access.rb
@@ -1,12 +1,7 @@
-# frozen_string_literal: true
-
 require_relative "rack/cloudflare_access/version"
 require "rack/cloudflare_access/railtie" if defined?(::Rails::Railtie)
 require "rack/cloudflare_access/middleware"
 require "logger"
-# require 'active_support/core_ext/object/blank' # Might be able to get away with not using it
-
-# Usage: Rails.application.config.middleware.use(Rack::CloudflareAccess::Middleware, aud, teams_url)
 
 module Rack
   module CloudflareAccess
